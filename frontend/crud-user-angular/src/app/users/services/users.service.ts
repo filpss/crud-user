6d8813b40127127eab5b63d9502953e050dabdaf
filpss <filipepinho2010@hotmail.com>
2024-08-15 02:31:05 -0300

    Criando serviço de deletar usuário

--- frontend/crud-user-angular/src/app/users/services/users.service.ts
+++ frontend/crud-user-angular/src/app/users/services/users.service.ts
@@ -1,29 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {User} from '../model/user';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UsersService {
 
     private readonly API = 'http://localhost:8001/users';
 
 
-  constructor(private httpClient: HttpClient) {
+    constructor(private httpClient: HttpClient) {
 
-  }
+    }
 
-  listAllUsers():Observable<User[]> {
-    return this.httpClient.get<User[]>(this.API);
-  }
+    listAllUsers(): Observable<User[]> {
+        return this.httpClient.get<User[]>(this.API);
+    }
 
-  getUserById(id: string): Observable<User> {
-      return this.httpClient.get<User>(`${this.API}/${id}`)
-  }
+    getUserById(id: string): Observable<User> {
+        return this.httpClient.get<User>(`${this.API}/${id}`);
+    }
 
-  addUser(user: User): Observable<User> {
-      return this.httpClient.post<User>(this.API, user);
-  }
+    addUser(user: User): Observable<User> {
+        return this.httpClient.post<User>(this.API, user);
+    }
+
+    deleteUser(id: string): Observable<void> {
+        return this.httpClient.delete<void>(`${this.API}/${id}`);
+    }
 }
